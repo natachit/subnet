e5aba2381bd53345c6463c284ac260d964036baa
natachit <natachit@hotmail.com>
2017-11-28 16:02:29 +0700

    convert ip ten to binary (not final)

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,17 @@
-export const plus = (x, y) => x + y;
+export const convertToSubnet = (mask) => {
+    const subnet = [0, 0, 0, 0].map(() => {
+        const sub = '00000000'.split('').map(() => {
+            mask -=1;
+            return mask >=0 ? '1' : '0';
+        })
+        return parseInt(sub.join(''), 2);
+    });
+    return subnet.join('.');
+}
+
+export const tenToBinary = (ip) => {
+    const ip2 = ip.split('.').map((elem) => {
+        return ((+elem).toString(2));
+    });
+    return ip2.join('.');
+}
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,8 +1,17 @@
-import { plus } from './helper';
+import { convertToSubnet, tenToBinary } from './helper';
 import { expect } from 'chai';
 
-describe('plus test', () => {
-    it('should return x + y', () => {
-        expect(plus(1, 2)).to.equal(3);
-    });
+describe('convert subnet test', () => {
+    it('convert subnet test', () => {
+        expect(convertToSubnet(32)).to.equal('255.255.255.255');
+        expect(convertToSubnet(1)).to.equal('128.0.0.0');
+    })
+})
+
+describe('convert ten to binary', () => {
+    it('convert ten to binary', () => {
+        expect(tenToBinary('255.255.255.255')).to.equal('11111111.11111111.11111111.11111111');
+        expect(tenToBinary('255.254.0.0')).to.equal('11111111.11111110.00000000.00000000');
+        expect(tenToBinary('255.248.0.0')).to.equal('11111111.11111000.00000000.00000000');
+    })
 })
