60883390704c7c902e5bed16aee827340b187eb7
natachit <natachit@hotmail.com>
2017-11-28 17:13:29 +0700

    network address

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -11,8 +11,18 @@ export const convertToSubnet = (mask) => {
 
 export const tenToBinary = (ip) => {
     const ip2 = ip.split('.').map((elem) => {
-        var tmp = (+elem).toString(2);
+        const tmp = (+elem).toString(2);
         return '0'.repeat(8-tmp.length) + tmp;
     });
     return ip2.join('.');
+}
+
+export const getNetworkAddress = (ip, mask) => {
+    ip = tenToBinary(ip).split('.').join('');
+    let output = ip.substr(0, mask)+'0'.repeat(32-mask);
+    output = output.match(/.{8}/g);
+    const netaddr = output.map((elem) => {
+        return parseInt((+elem),2)
+    })
+    return netaddr.join('.');
 }
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,4 +1,4 @@
-import { convertToSubnet, tenToBinary } from './helper';
+import { convertToSubnet, tenToBinary, getNetworkAddress } from './helper';
 import { expect } from 'chai';
 
 describe('convert subnet test', () => {
@@ -13,5 +13,13 @@ describe('convert ten to binary', () => {
         expect(tenToBinary('255.255.255.255')).to.equal('11111111.11111111.11111111.11111111');
         expect(tenToBinary('255.254.0.0')).to.equal('11111111.11111110.00000000.00000000');
         expect(tenToBinary('255.248.0.0')).to.equal('11111111.11111000.00000000.00000000');
+    })
+})
+
+describe('Get Network Address', () => {
+    it('Get Network Address', () => {
+        expect(getNetworkAddress('192.168.1.204',16)).to.equal('192.168.0.0');
+        expect(getNetworkAddress('158.108.12.34',24)).to.equal('158.108.12.0');
+        expect(getNetworkAddress('158.108.12.34',19)).to.equal('158.108.0.0');
     })
 })
